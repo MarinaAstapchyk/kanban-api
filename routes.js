7a08b928eb58df94b898722a3f6accaeca0f9948
Marina <marinanesyaeva@gmail.com>
2023-05-24 13:49:40 -0700

    new commit

--- routes.js
+++ routes.js
@@ -12,26 +12,26 @@ function routes(app) {
 
   app.get('/cards', (req, res) => {
     res.send(cards);
-  })
+  });
 
   app.delete('/cards/:cardId', (req, res) => {
     const cardId = req.params.cardId;
     cards = cards.filter(el => el.id !== cardId);
     res.send(cards);
-  })
+  });
 
   app.post('/cards', (req, res) => {
     const card = req.body;
     cards.push({id: Math.random().toString(), ...card});
     res.send('Card created');
-  })
+  });
 
   app.patch('/cards/:cardId', (req, res) => {
     const cardId = req.params.cardId;
     const card = req.body;
     cards = cards.map(el => el.id === cardId ? ({...card, id: el.id}) : el);
     res.send('Card updated');
-  })
+  });
 }
 
 module.exports = routes;
